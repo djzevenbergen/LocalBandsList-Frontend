514c3b41e5a480afaecb6c7c9150dc4277255ca8
DJ Zevenbergen <djzevenbergen@gmail.com>
2020-07-17 14:54:30 -0700

    begin making API call

---
+++ src/components/BandList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Bands extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      bands: []
+    };
+  }
+
+  render() {
+
+  }
+}
+
+export default Bands;
